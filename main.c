ebae684ac175bbc7f25b28a084ccd775254d7e38
Suteki <a3234979817@outlook.com>
2026-03-29 20:57:17 +0800

    实现了一些基础功能

--- main.c
+++ main.c
@@ -39,37 +39,68 @@ struct RCC{
 //定义RCC初地址
 #define RCC ((volatile struct RCC*)0x40021000)
 
+//定义位操作
+#define SET_BIT(REG,BIT) ((REG) |= (BIT))//BIT为0的位不变，为1的位设置为1
+#define CLEAN_BIT(REG,BIT) ((REG) &= ~(BIT))//BIT为0的位不变，为1的位清空为0
+#define TOGGLE_BIT(REG,BIT) ((REG) ^= (BIT))//BIT为0的位不变，为1的位切换状态
+#define READ_BIT(REG,BIT) ((REG) & (BIT))//检查REG中对应的BIT设置为1的位的值
+
+void init_gpioa(int pin_num)//pin_num为引脚号，范围0～15
+{
+    if (pin_num != 13 && pin_num != 14 && pin_num != 15)
+    {
+        SET_BIT((RCC->APB2ENR),(1 << 2)); 
+        if (pin_num >=0 && pin_num <= 7)
+        {
+            CLEAN_BIT((GPIOA->CRL),(0xF << (pin_num * 4)));
+            SET_BIT((GPIOA->CRL),(0x3 << (pin_num * 4)));
+        }
+        if (pin_num >= 8 && pin_num <= 15)
+        {
+            CLEAN_BIT((GPIOA->CRH),(0xF << ((pin_num - 8) * 4)));
+            SET_BIT((GPIOA->CRH),(0x3 << ((pin_num - 8) * 4)));
+        }
+    }
+}
+
+void set_gpioa(int pin_num,char level)//pin_num为引脚号，范围0～15；level为电平，h高l低
+{
+    if (level == 'l')
+    {
+        CLEAN_BIT((GPIOA->ODR),(1 << pin_num));
+    }
+    if (level == 'h')
+    {
+        SET_BIT((GPIOA->ODR),(1 << pin_num));
+    }
+}
+
 void delay(int n) {
     while(n--) { __asm("nop"); } // 简单的软件延时
 }
 
-// 骗过链接器：告诉它 SystemInit 在这，虽然它什么都不干
+// 骗过链接器
 void SystemInit(void) {}
-// 骗过链接器：告诉它 C 库初始化在这
 void __libc_init_array(void) {}
 
+//PC13
+#define RCC_APB2ENR (*(volatile unsigned int*)0x40021018) 
+#define GPIOC_CRH (*(volatile unsigned int*)0x40011004)
+#define GPIOC_ODR (*(volatile unsigned int*)0x4001100C)
+
 int main(void) {
-    //开启所有端口的RCC_APB2ENR
-    RCC->APB2ENR |= (0x7F << 2);
-
-    /* 目标 B：配置 PC13 为推挽输出模式 (50MHz) */
-    // PC13 对应 CRH 寄存器的 [23:20] 这四位
-    // 0x3 代表输出模式 50MHz, 0x0 代表通用推挽
-    GPIOC->CRH &= ~(0xF << 20); // 先清空这 4 位
-    GPIOC->CRH |= (0x3 << 20);  // 填入二进制 0011 (即 0x3)
-    GPIOA->CRL &= ~(0xF << 4);
-    GPIOA->CRL |= (0x3 << 4);
-
-    /* 目标 C：进入死循环，让灯闪烁 */
+    SET_BIT((RCC_APB2ENR),(1 << 4));
+    CLEAN_BIT((GPIOC_CRH),(0xF << 20));
+    SET_BIT((GPIOC_CRH),(0x3 << 20));
+    init_gpioa(10);
     while(1) {
-        // 输出低电平 (Bit 13 写 0) -> LED 亮
-        GPIOC->ODR &= ~(1 << 13);
-        GPIOA->ODR &= ~(1 << 1);
+        CLEAN_BIT((GPIOC_ODR),(1 << 13));
+        char level = 'l';
+        set_gpioa(10,level);
         delay(100000);
-
-        // 输出高电平 (Bit 13 写 1) -> LED 灭
-        GPIOC->ODR |= (1 << 13);
-        GPIOA->ODR |= (1 << 1);
+        SET_BIT((GPIOC_ODR),(1 << 13));
+        level = 'h';
+        set_gpioa(10,level);
         delay(100000);
     }
 }
