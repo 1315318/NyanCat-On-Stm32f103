5fdb19ad4ff9e34b84125d6190d33d40301bb32b
Suteki <a3234979817@outlook.com>
2026-04-02 17:04:33 +0800

    基础框架

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ PB3 (JTDO)：JTAG 专用
 PB4 (NJTRST)：JTAG 专用*/
 
 //定义GPIO端口寄存器
-struct GPIOx{
+struct GPIOx
+{
     volatile unsigned int CRL;
     volatile unsigned int CRH;
     volatile unsigned int IDR;
@@ -24,7 +25,8 @@ struct GPIOx{
 #define GPIOG ((volatile struct GPIOx*)0x40012000)
 
 //定义RCC寄存器
-struct RCC{
+struct RCC
+{
     volatile unsigned int CR;
     volatile unsigned int CFGR;
     volatile unsigned int CIR;
@@ -39,26 +41,56 @@ struct RCC{
 //定义RCC初地址
 #define RCC ((volatile struct RCC*)0x40021000)
 
+//定义SysTick定时器
+struct SysTick
+{
+    volatile unsigned int CAS;//控制及状态寄存器
+    volatile unsigned int RV;//重装载数值寄存器
+    volatile unsigned int CV;//当前数值寄存器
+    volatile unsigned int CALV;//校准数值寄存器
+};
+//定义Sys宏
+#define SysTick ((volatile struct SysTick*)0xE000E010)
+
 //定义位操作
 #define SET_BIT(REG,BIT) ((REG) |= (BIT))//BIT为0的位不变，为1的位设置为1
 #define CLEAN_BIT(REG,BIT) ((REG) &= ~(BIT))//BIT为0的位不变，为1的位清空为0
 #define TOGGLE_BIT(REG,BIT) ((REG) ^= (BIT))//BIT为0的位不变，为1的位切换状态
 #define READ_BIT(REG,BIT) ((REG) & (BIT))//检查REG中对应的BIT设置为1的位的值
 
-void init_gpioa(int pin_num)//pin_num为引脚号，范围0～15
+//定义输出模式宏
+#define GPIO_MODE_PP 0
+#define GPIO_MODE_OL 1
+void init_gpioa(int pin_num,int mode)//pin_num为引脚号，范围0～15；mode为输出模式，0为推挽，1为开漏
 {
     if (pin_num != 13 && pin_num != 14 && pin_num != 15)
     {
         SET_BIT((RCC->APB2ENR),(1 << 2)); 
-        if (pin_num >=0 && pin_num <= 7)
+        if (mode == GPIO_MODE_PP)//推挽输出
         {
-            CLEAN_BIT((GPIOA->CRL),(0xF << (pin_num * 4)));
-            SET_BIT((GPIOA->CRL),(0x3 << (pin_num * 4)));
+            if (pin_num >=0 && pin_num <= 7)
+            {
+                CLEAN_BIT((GPIOA->CRL),(0xF << (pin_num * 4)));
+                SET_BIT((GPIOA->CRL),(0x3 << (pin_num * 4)));
+            }
+            if (pin_num >= 8 && pin_num <= 15)
+            {
+                CLEAN_BIT((GPIOA->CRH),(0xF << ((pin_num - 8) * 4)));
+                SET_BIT((GPIOA->CRH),(0x3 << ((pin_num - 8) * 4)));
+            }
         }
-        if (pin_num >= 8 && pin_num <= 15)
+            if (mode == GPIO_MODE_OL)//开漏输出
         {
-            CLEAN_BIT((GPIOA->CRH),(0xF << ((pin_num - 8) * 4)));
-            SET_BIT((GPIOA->CRH),(0x3 << ((pin_num - 8) * 4)));
+            if (pin_num >=0 && pin_num <= 7)
+            {
+                CLEAN_BIT((GPIOA->CRL),(0xF << (pin_num * 4)));
+                SET_BIT((GPIOA->CRL),(0x7 << (pin_num * 4)));
+            }
+            if (pin_num >= 8 && pin_num <= 15)
+            {
+                CLEAN_BIT((GPIOA->CRH),(0xF << ((pin_num - 8) * 4)));
+                SET_BIT((GPIOA->CRH),(0x7 << ((pin_num - 8) * 4)));
+            }
         }
     }
 }
@@ -75,8 +107,13 @@ void set_gpioa(int pin_num,char level)//pin_num为引脚号，范围0～15；lev
     }
 }
 
-void delay(int n) {
-    while(n--) { __asm("nop"); } // 简单的软件延时
+void systick_timing(int time)
+{
+    CLEAN_BIT((SysTick->CAS),(1));
+    CLEAN_BIT((SysTick->RV),(0x1FFFFFF));
+    SysTick->CV = 0;
+    SysTick->RV = time;
+    SET_BIT((SysTick->CAS),(1));
 }
 
 // 骗过链接器
@@ -88,19 +125,13 @@ void __libc_init_array(void) {}
 #define GPIOC_CRH (*(volatile unsigned int*)0x40011004)
 #define GPIOC_ODR (*(volatile unsigned int*)0x4001100C)
 
-int main(void) {
+int main(void) 
+{
+    //PC13
     SET_BIT((RCC_APB2ENR),(1 << 4));
     CLEAN_BIT((GPIOC_CRH),(0xF << 20));
     SET_BIT((GPIOC_CRH),(0x3 << 20));
-    init_gpioa(10);
-    while(1) {
-        CLEAN_BIT((GPIOC_ODR),(1 << 13));
-        char level = 'l';
-        set_gpioa(10,level);
-        delay(100000);
-        SET_BIT((GPIOC_ODR),(1 << 13));
-        level = 'h';
-        set_gpioa(10,level);
-        delay(100000);
-    }
+    //GPIO端口初始化
+    init_gpioa(7,GPIO_MODE_OL);
+    init_gpioa(9,GPIO_MODE_OL);
 }
