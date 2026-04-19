9d0894bfe1092ca83d2dfd8298ef0cd7928af69d
Suteki <a3234979817@outlook.com>
2026-04-19 11:30:00 +0800

    重写DMA寄存器映射

--- dma.h
+++ dma.h
@@ -1,18 +1,32 @@
 #ifndef DMA_H
 #define DMA_H
 
+struct DMA_CHANNEL
+{
+    volatile unsigned int CCR;
+    volatile unsigned int CNDTR;
+    volatile unsigned int CPAR;
+    volatile unsigned int CMAR;
+};
+
 struct DMA
 {
     volatile unsigned int ISR;
     volatile unsigned int IFCR;
-    volatile unsigned int CCRx;
-    volatile unsigned int CNDTRx;
-    volatile unsigned int CPARx;
-    volatile unsigned int CMARx;
+    sturct DMA_CHANNEL[7]; //DMA1有7个通道
 };
 
 //定义DMA初地址
 #define DMA ((volatile struct DMA*)0x40020000) 
+
+//定义DMA通道宏
+#define DMA_CH1 0
+#define DMA_CH2 1
+#define DMA_CH3 2
+#define DMA_CH4 3
+#define DMA_CH5 4
+#define DMA_CH6 5
+#define DMA_CH7 6
 
 void enr_dma(void)
 {
